280b33824388ccf41279e14c9e948bd09cd2dad4
Miri <paletaleblue@outlook.com>
2025-08-30 23:13:24 -0600

    Parte 4. Implementacion de lazy load

---
+++ gestion-usuarios/src/app/pages/documentacion/documentacion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documentacion',
+  imports: [],
+  templateUrl: './documentacion.html',
+  styleUrl: './documentacion.css'
+})
+export class Documentacion {
+
+}
---
+++ gestion-usuarios/src/app/pages/pages-module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Perfil } from './perfil/perfil';
+import { Home } from './home/home';
+import { Documentacion } from './documentacion/documentacion';
+import { Estructuras } from './estructuras/estructuras';
+import { Usuarios } from './usuarios/usuarios';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'documentacion',
+    component: Documentacion
+  },
+  {
+    path: 'estructuras',
+    component: Estructuras
+  },
+  {
+    path: 'usuarios',
+    component: Usuarios
+  },
+  {
+    path: 'perfil',
+    component: Perfil
+  },
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Home,Documentacion,Estructuras,Usuarios,Perfil,
+    RouterModule.forChild(routes)
+  ]
+})
+export class PagesModule { }
